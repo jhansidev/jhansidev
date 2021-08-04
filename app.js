d3a46df8b40d97a8d3d6cd5e397b210561b0d8f2
Jhansi Rani <jhansi@Jhansis-MacBook-Air.local>
2021-08-04 16:49:46 -0400

    Blog page added

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-$("#button").click(function(){
-    $("#nav").toggleClass("hide");
-    $("i").toggleClass("active");
-});
+void function() {
+    window.__onThemeChange = function() {}
+    var preferredTheme
+
+    try {
+    preferredTheme = localStorage.getItem('theme')
+} catch (err) { }
+
+    function setTheme(newTheme) {
+    if (preferredTheme && document.body.classList.contains(preferredTheme)) {
+    document.body.classList.replace(preferredTheme, newTheme)
+} else {
+    document.body.classList.add(newTheme)
+}
+
+    window.__theme = newTheme
+    preferredTheme = newTheme
+    window.__onThemeChange(newTheme)
+}
+
+    window.__setPreferredTheme = function(newTheme) {
+    setTheme(newTheme)
+    try {
+    localStorage.setItem('theme', newTheme)
+} catch (err) {}
+}
+
+    var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    darkQuery.addEventListener(function(e){
+        window.__setPreferredTheme(e.matches?"dark":"light")
+})
+
+    setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+}()
